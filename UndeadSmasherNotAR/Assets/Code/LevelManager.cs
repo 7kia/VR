2622327ee6e24cc4fb39d51ee433fdc8c82ada14
7kia <il7kia@yandex.ru>
2017-12-02 16:15:58 +0400

    Добавлен тестовый проект без поддержки AR

---
+++ UndeadSmasherNotAR/Assets/Code/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Code;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Code
+{
+    public class LevelManager : MonoBehaviour
+    {
+
+        public MapLoader m_mapLoader;
+        // Use this for initialization
+        void Start()
+        {
+            m_mapLoader.LoadMap("Maps\\Levels.xml");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ UndeadSmasherNotAR/Assets/Code/UndeadSmasherObjectFactory.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UndeadSmasherObjectFactory : MonoBehaviour {
+
+    public GameObject spawnLocations;
+    public Vector3 scaleFactorForBlocks;
+
+
+    [SerializeField]
+    public GameObject[] objectList;
+    [SerializeField]
+    public GameObject[] blockList;
+
+    private Dictionary<string, GameObject> objectMap = new Dictionary<string, GameObject>();
+    private Dictionary<string, GameObject> blockMap = new Dictionary<string, GameObject>();
+    private bool m_createNameMap = false;
+
+    public void CreateNameMap()
+    {
+        for(int i = 0; i < objectList.Length; i++)
+        {
+            if (!objectMap.ContainsKey(objectList[i].name))
+            {
+                objectMap.Add(objectList[i].name, objectList[i]);
+            }
+        }
+
+        for (int i = 0; i < blockList.Length; i++)
+        {
+            if (!blockMap.ContainsKey(blockList[i].name))
+            {
+                blockMap.Add(blockList[i].name, blockList[i]);
+            }
+        }
+    }
+
+    public void CreateObject(Vector3 position, String nameObject)
+    {
+
+        if(!m_createNameMap)
+        {
+            m_createNameMap = false;
+            CreateNameMap();
+        }
+
+        GameObject newObject = Instantiate(
+            objectMap[nameObject],
+            spawnLocations.transform.position,
+            Quaternion.Euler(0, 0, 0)
+        ) as GameObject;
+
+        newObject.transform.parent = spawnLocations.transform;
+        newObject.transform.position = position;
+
+
+        Debug.Log("GameObject Name = " + name + " position={" + newObject.transform.position.x + ", " + newObject.transform.position.y + ", " + newObject.transform.position.z + "}\n");
+
+    }
+
+    public void CreateBlock(Vector3 position, String nameBlock)
+    {
+        if (!m_createNameMap)
+        {
+            m_createNameMap = false;
+            CreateNameMap();
+        }
+
+        GameObject newBlock = Instantiate(
+            blockMap[nameBlock],
+            spawnLocations.transform.position,
+            Quaternion.Euler(0, 0, 0)
+        ) as GameObject;
+
+        newBlock.transform.parent = spawnLocations.transform;
+
+        Vector3 absolutePosition = new Vector3(
+            spawnLocations.transform.position.x + (position.x * scaleFactorForBlocks.x),
+            spawnLocations.transform.position.y + (position.y * scaleFactorForBlocks.y),
+            spawnLocations.transform.position.z + (position.z * scaleFactorForBlocks.z)
+        );
+        newBlock.transform.position = absolutePosition;
+    }
+
+}
